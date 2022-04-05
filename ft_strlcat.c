5c734158856840f0699ff71fd1220c96ae5aa5c4
Arnaud Halleux <ahalleux@fu-r10-p1.s19.be>
2022-04-05 16:07:57 +0200

    update

---
+++ ft_strlcat.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahalleux <ahalleux@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/05 12:48:00 by ahalleux          #+#    #+#             */
+/*   Updated: 2022/04/05 15:12:34 by ahalleux         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dest, const char *src, size_t destsize)
+{
+	size_t		i;
+	size_t		j;
+
+	i = 0;
+	j = 0;
+	i = ft_strlen(dest);
+	j = ft_strlen(src);
+	if (destsize < i)
+	{
+		return (destsize + j);
+	}
+	while (destsize > 0 && i < destsize - 1 && src[j])
+		dest[i++] = src[j++];
+	dest[i] = '\0';
+	while (src[j++])
+		i++;
+	return (i);
+}
